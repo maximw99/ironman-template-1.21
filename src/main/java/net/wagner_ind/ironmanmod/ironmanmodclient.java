f435e70e68a3a514d10cec84dceab690f43e65a9
Maxim <MaximWagnerWalldorf@live.de>
2024-11-27 17:19:22 +0100

    init

---
+++ src/main/java/net/wagner_ind/ironmanmod/ironmanmodclient.java
@@ -0,0 +1,10 @@
+package net.wagner_ind.ironmanmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ironmanmodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
